4ab44cb4288fbbbdf1c737e8c6e2f07bf09a6fed
ValloonSweet <valloon91105@gmail.com>
2024-04-24 12:00:00 -0700

    speed up using channel

--- Pipeline using channel/windDirection.go
+++ Pipeline using channel/windDirection.go
@@ -22,53 +22,72 @@ var (
 	windDist      [8]int
 )
 
-func parseToArray(text string) []string {
-	lines := strings.Split(text, "\n")
-	metarSlice := make([]string, 0, len(lines))
-	metarStr := ""
+func parseToArray(textChannel chan string, metarChannel chan []string) {
+	for text := range textChannel {
+		lines := strings.Split(text, "\n")
+		metarSlice := make([]string, 0, len(lines))
+		metarStr := ""
 
-	for _, line := range lines {
-		if tarValidation.MatchString(line) {
-			break
-		}
-		if !comment.MatchString(line) {
-			metarStr += strings.Trim(line, " ")
-		}
-		if metarClose.MatchString(line) {
-			metarSlice = append(metarSlice, metarStr)
-			metarStr = ""
-		}
-	}
-	return metarSlice
-}
-
-func extractWindDirection(metars []string) []string {
-	winds := make([]string, 0, len(metars))
-	for _, metar := range metars {
-		if windRegex.MatchString(metar) {
-			winds = append(winds, windRegex.FindAllStringSubmatch(metar, -1)[0][1])
-		}
-	}
-	return winds
-}
-
-func mineWindDistribution(winds []string) {
-	for _, wind := range winds {
-		if variableWind.MatchString(wind) {
-			for i := 0; i < 8; i++ {
-				windDist[i]++
+		for _, line := range lines {
+			if tarValidation.MatchString(line) {
+				break
 			}
-		} else if validWind.MatchString(wind) {
-			windStr := windDirOnly.FindAllStringSubmatch(wind, -1)[0][1]
-			if d, err := strconv.ParseFloat(windStr, 64); err == nil {
-				dirIndex := int(math.Round(d/45.0)) % 8
-				windDist[dirIndex]++
+			if !comment.MatchString(line) {
+				metarStr += strings.Trim(line, " ")
+			}
+			if metarClose.MatchString(line) {
+				metarSlice = append(metarSlice, metarStr)
+				metarStr = ""
 			}
 		}
+		metarChannel <- metarSlice
 	}
+	close(metarChannel)
+}
+
+func extractWindDirection(metarsChannel chan []string, windChannel chan []string) {
+	for metars := range metarsChannel {
+		winds := make([]string, 0, len(metars))
+		for _, metar := range metars {
+			if windRegex.MatchString(metar) {
+				winds = append(winds, windRegex.FindAllStringSubmatch(metar, -1)[0][1])
+			}
+		}
+		windChannel <- winds
+	}
+	close(windChannel)
+}
+
+func mineWindDistribution(windsChannel chan []string, resultChannel chan [8]int) {
+	for winds := range windsChannel {
+		for _, wind := range winds {
+			if variableWind.MatchString(wind) {
+				for i := 0; i < 8; i++ {
+					windDist[i]++
+				}
+			} else if validWind.MatchString(wind) {
+				windStr := windDirOnly.FindAllStringSubmatch(wind, -1)[0][1]
+				if d, err := strconv.ParseFloat(windStr, 64); err == nil {
+					dirIndex := int(math.Round(d/45.0)) % 8
+					windDist[dirIndex]++
+				}
+			}
+		}
+	}
+	resultChannel <- windDist
+	close(resultChannel)
 }
 
 func main() {
+	textChannel := make(chan string)
+	metarChannel := make(chan []string)
+	windsChannel := make(chan []string)
+	resultChannel := make(chan [8]int)
+
+	go parseToArray(textChannel, metarChannel)
+	go extractWindDirection(metarChannel, windsChannel)
+	go mineWindDistribution(windsChannel, resultChannel)
+
 	absPath, _ := filepath.Abs("./metarfiles/")
 	files, _ := os.ReadDir(absPath)
 	start := time.Now()
@@ -79,13 +98,11 @@ func main() {
 			panic(err)
 		}
 		text := string(dat)
-
-		metarsReports := parseToArray(text)
-
-		windsDirections := extractWindDirection(metarsReports)
-
-		mineWindDistribution(windsDirections)
+		textChannel <- text
 	}
+	close(textChannel)
+	windDist := <-resultChannel
+
 	elapsed := time.Since(start)
 	fmt.Println(windDist)
 	fmt.Println("Processing took ", elapsed)

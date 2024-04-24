6e092b706075e9c07cd0d7a83bee454b8ecf3adc
ValloonSweet <valloon91105@gmail.com>
2024-04-24 10:16:35 -0700

    wind processing without channel

---
+++ Pipeline using channel/windDirection.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	windRegex     = regexp.MustCompile(`\d* METAR.*EGLL \d*Z [A-Z ]*(\d{5}KT|VRB\d{2}KT).*=`)
+	tarValidation = regexp.MustCompile(`.*TFA.*`)
+	comment       = regexp.MustCompile(`\w*#.*`)
+	metarClose    = regexp.MustCompile(`.*=`)
+	variableWind  = regexp.MustCompile(`.*VRB\d{2}KT`)
+	validWind     = regexp.MustCompile(`\d{5}KT`)
+	windDirOnly   = regexp.MustCompile(`(\d{3})\d{2}KT`)
+	windDist      [8]int
+)
+
+func parseToArray(text string) []string {
+	lines := strings.Split(text, "\n")
+	metarSlice := make([]string, 0, len(lines))
+	metarStr := ""
+
+	for _, line := range lines {
+		if tarValidation.MatchString(line) {
+			break
+		}
+		if !comment.MatchString(line) {
+			metarStr += strings.Trim(line, " ")
+		}
+		if metarClose.MatchString(line) {
+			metarSlice = append(metarSlice, metarStr)
+			metarStr = ""
+		}
+	}
+	return metarSlice
+}
+
+func extractWindDirection(metars []string) []string {
+	winds := make([]string, 0, len(metars))
+	for _, metar := range metars {
+		if windRegex.MatchString(metar) {
+			winds = append(winds, windRegex.FindAllStringSubmatch(metar, -1)[0][1])
+		}
+	}
+	return winds
+}
+
+func mineWindDistribution(winds []string) {
+	for _, wind := range winds {
+		if variableWind.MatchString(wind) {
+			for i := 0; i < 8; i++ {
+				windDist[i]++
+			}
+		} else if validWind.MatchString(wind) {
+			windStr := windDirOnly.FindAllStringSubmatch(wind, -1)[0][1]
+			if d, err := strconv.ParseFloat(windStr, 64); err == nil {
+				dirIndex := int(math.Round(d/45.0)) % 8
+				windDist[dirIndex]++
+			}
+		}
+	}
+}
+
+func main() {
+	absPath, _ := filepath.Abs("./metarfiles/")
+	files, _ := os.ReadDir(absPath)
+	start := time.Now()
+
+	for _, file := range files {
+		dat, err := os.ReadFile(filepath.Join(absPath, file.Name()))
+		if err != nil {
+			panic(err)
+		}
+		text := string(dat)
+
+		metarsReports := parseToArray(text)
+
+		windsDirections := extractWindDirection(metarsReports)
+
+		mineWindDistribution(windsDirections)
+	}
+	elapsed := time.Since(start)
+	fmt.Println(windDist)
+	fmt.Println("Processing took ", elapsed)
+}

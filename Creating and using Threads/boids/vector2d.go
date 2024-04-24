8854b916fba3061a9fa183d14e2682e3b40498dc
ValloonSweet <valloon91105@gmail.com>
2024-04-23 20:00:07 -0700

    fix: vector operator

--- Creating and using Threads/boids/vector2d.go
+++ Creating and using Threads/boids/vector2d.go
@@ -8,32 +8,31 @@ type Vector2D struct {
 }
 
 func (v1 Vector2D) Add(v2 Vector2D) Vector2D {
-	return Vector2D{x: v1.x + v2.x, y: v1.y + v2.y}
+	return Vector2D{v1.x + v2.x, v1.y + v2.y}
 }
 
 func (v1 Vector2D) Subtract(v2 Vector2D) Vector2D {
-	return Vector2D{x: v1.x + v2.x, y: v1.y + v2.y}
+	return Vector2D{v1.x - v2.x, v1.y - v2.y}
 }
 
 func (v1 Vector2D) Multiply(v2 Vector2D) Vector2D {
-	return Vector2D{x: v1.x * v2.x, y: v1.y * v2.y}
+	return Vector2D{v1.x * v2.x, v1.y * v2.y}
 }
 
 func (v1 Vector2D) AddV(d float64) Vector2D {
-	return Vector2D{x: v1.x + d, y: v1.y + d}
+	return Vector2D{v1.x + d, v1.y + d}
 }
 
 func (v1 Vector2D) MultiplyV(d float64) Vector2D {
-	return Vector2D{x: v1.x * d, y: v1.y * d}
+	return Vector2D{v1.x * d, v1.y * d}
 }
 
 func (v1 Vector2D) DivisionV(d float64) Vector2D {
-	return Vector2D{x: v1.x / d, y: v1.y / d}
+	return Vector2D{v1.x / d, v1.y / d}
 }
 
 func (v1 Vector2D) limit(lower, upper float64) Vector2D {
-	return Vector2D{x: math.Min(math.Max(v1.x, lower), upper),
-		y: math.Min(math.Max(v1.y, lower), upper)}
+	return Vector2D{math.Min(math.Max(v1.x, lower), upper), math.Min(math.Max(v1.y, lower), upper)}
 }
 
 func (v1 Vector2D) Distance(v2 Vector2D) float64 {

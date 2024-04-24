95341c30cb2c0bbac1b46c377045b8a81a834abb
ValloonSweet <valloon91105@gmail.com>
2024-04-24 14:07:07 -0700

    feat: thread pool

---
+++ Thread pool/go.mod
@@ -0,0 +1,3 @@
+module threadpool
+
+go 1.21.5
---
+++ Thread pool/showlace.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+type Point2D struct {
+	x int
+	y int
+}
+
+const numberOfThreads = 8
+
+var (
+	r         = regexp.MustCompile(`\((\d*),(\d*)\)`)
+	waitGroup = sync.WaitGroup{}
+)
+
+func findArea(inputChannel chan string) {
+	for pointStr := range inputChannel {
+		var points []Point2D
+		for _, p := range r.FindAllStringSubmatch(pointStr, -1) {
+			x, _ := strconv.Atoi(p[1])
+			y, _ := strconv.Atoi(p[2])
+			points = append(points, Point2D{x, y})
+		}
+
+		area := 0.0
+
+		for i := 0; i < len(points); i++ {
+			a, b := points[i], points[(i+1)%len(points)]
+			area += float64(a.x*b.y) - float64(a.y*b.x)
+		}
+		fmt.Println(math.Abs(area) / 2.0)
+	}
+	waitGroup.Done()
+}
+
+func main() {
+	absPath, _ := filepath.Abs("./")
+	dat, _ := os.ReadFile(filepath.Join(absPath, "polygons.txt"))
+	text := string(dat)
+
+	inputChannel := make(chan string, 1000)
+	for i := 0; i < numberOfThreads; i++ {
+		go findArea(inputChannel)
+	}
+	waitGroup.Add(numberOfThreads)
+	start := time.Now()
+	for _, line := range strings.Split(text, "\n") {
+		inputChannel <- line
+	}
+	close(inputChannel)
+	waitGroup.Wait()
+	elapsed := time.Since(start)
+	fmt.Println("Processing took ", elapsed)
+}
